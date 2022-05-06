d8c6ce9c3cd1361c3278d58fa1f38363b8b9d04a
SP <SP@DESKTOP-V5AC4TG>
2022-05-06 08:12:03 +0530

    First commit

---
+++ src/first/Test.java
@@ -0,0 +1,16 @@
+package first;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		
+		int a = 10;
+		int b = 20;
+		
+		System.out.println(a+b);
+		
+	}
+
+}
