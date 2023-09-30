5191b823c0ce4b7b252e62f837d1fcdf5b06e983
sp-code <saurabhpurandare1@gmail.com>
2023-09-30 17:49:07 +0530

    Addition & subtraction

--- src/first/Test.java
+++ src/first/Test.java
@@ -9,7 +9,8 @@ public class Test {
 		int a = 10;
 		int b = 20;
 		
-		System.out.println(a+b);
+		System.out.println("Addition is "+(a+b));
+		System.out.println("Subtraction is "+(a-b));
 		
 	}
 
